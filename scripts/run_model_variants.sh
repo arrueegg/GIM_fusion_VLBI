2e3b6a3e88fe381ea24b81939d31ca088c822299
arrueegg <arrueegg@student.ethz.ch>
2024-12-05 13:40:55 +0100

    added echos

--- scripts/run_model_variants.sh
+++ scripts/run_model_variants.sh
@@ -2,42 +2,53 @@
 
 cd /scratch2/arrueegg/WP2/GIM_fusion_VLBI/
 
-#python src/train.py --debug False --year 2023 --doy 010 --mode GNSS --loss_fn LaplaceLoss
-#python src/inference_map.py --debug False --year 2023 --doy 010 --mode GNSS --loss_fn LaplaceLoss
+echo "Running GNSS with LaplaceLoss"
+python src/train.py --debug False --year 2023 --doy 010 --mode GNSS --loss_fn LaplaceLoss
+python src/inference_map.py --debug False --year 2023 --doy 010 --mode GNSS --loss_fn LaplaceLoss
 
+echo "Running GNSS with MSELoss"
 python src/train.py --debug False --year 2023 --doy 010 --mode GNSS --loss_fn MSELoss
 python src/inference_map.py --debug False --year 2023 --doy 010 --mode GNSS --loss_fn MSELoss
 
+echo "Running GNSS with GaussianNLLLoss"
 python src/train.py --debug False --year 2023 --doy 010 --mode GNSS --loss_fn GaussianNLLLoss
 python src/inference_map.py --debug False --year 2023 --doy 010 --mode GNSS --loss_fn GaussianNLLLoss
 
+echo "Running Fusion with LaplaceLoss"
 python src/train.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss
 python src/inference_map.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss
 
+echo "Running Fusion with LaplaceLoss and vlbi_loss_weight 10.0"
 python src/train.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_loss_weight 10.0
 python src/inference_map.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_loss_weight 10.0
 
+echo "Running Fusion with LaplaceLoss and vlbi_loss_weight 100.0"
 python src/train.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_loss_weight 100.0
 python src/inference_map.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_loss_weight 100.0
 
+echo "Running Fusion with LaplaceLoss and vlbi_loss_weight 1000.0"
 python src/train.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_loss_weight 1000.0
 python src/inference_map.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_loss_weight 1000.0
 
+echo "Running Fusion with LaplaceLoss and vlbi_loss_weight 10000.0"
 python src/train.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_loss_weight 10000.0
 python src/inference_map.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_loss_weight 10000.0
 
+echo "Running Fusion with LaplaceLoss and vlbi_sampling_weight 10.0"
 python src/train.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_sampling_weight 10.0
 python src/inference_map.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_sampling_weight 10.0
 
+echo "Running Fusion with LaplaceLoss and vlbi_sampling_weight 100.0"
 python src/train.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_sampling_weight 100.0
 python src/inference_map.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_sampling_weight 100.0
 
+echo "Running Fusion with LaplaceLoss and vlbi_sampling_weight 1000.0"
 python src/train.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_sampling_weight 1000.0
 python src/inference_map.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_sampling_weight 1000.0
 
+echo "Running Fusion with LaplaceLoss and vlbi_sampling_weight 10000.0"
 python src/train.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_sampling_weight 10000.0
 python src/inference_map.py --debug False --year 2023 --doy 010 --mode Fusion --loss_fn LaplaceLoss --vlbi_sampling_weight 10000.0
-
 
 # Wait for all background processes to finish
 wait
