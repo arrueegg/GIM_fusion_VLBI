e3a2b693ac11df581a7c3cb1c79f3fff327b0971
arrueegg <arrueegg@student.ethz.ch>
2025-01-10 13:19:45 +0100

    removed comments

--- scripts/submit_cluster.sh
+++ scripts/submit_cluster.sh
@@ -6,11 +6,6 @@
 #SBATCH --mem-per-cpu=1024
 #SBATCH --gpus=1
 
-# config over #
-%"#SBATCH --output=/dev/null"
-%"#SBATCH --output=output_files/output.out"
-
-
 module load stack/2024-06 python_cuda/3.11.6
 module load eth_proxy
 
