5ffecbc8c7cff2d28a762ed24ce7e697f4f26d78
arrueegg <arrueegg@student.ethz.ch>
2025-01-17 10:30:30 +0100

    updated but incomplete SA plots

--- scripts/eval_SA_results.py
+++ scripts/eval_SA_results.py
@@ -2,6 +2,7 @@
 import os
 import yaml
 import pandas as pd
+import numpy as np
 
 import matplotlib.pyplot as plt
 #import seaborn as sns
@@ -62,29 +63,57 @@ def evaluate(df):
         df_mode = df.groupby('data_mode')
         plt.figure(figsize=(14, 6))
 
+        # Define consistent bins and range for RMSE
+        rmse_min, rmse_max = df['RMSE'].min(), df['RMSE'].max()
+        mae_min, mae_max = df['MAE'].min(), df['MAE'].max()
+        
+        bins_rmse = np.linspace(rmse_min, rmse_max, 20)  # 30 bins for RMSE
+        bins_mae = np.linspace(mae_min, mae_max, 20)     # 30 bins for MAE
+
         plt.subplot(1, 2, 1)
         for name, group in df_mode:
-            plt.hist(group['RMSE'], alpha=0.5, label=name)
+            plt.hist(group['RMSE'], bins=bins_rmse, alpha=0.5, label=name, edgecolor='black')
         plt.title('RMSE by Data Mode')
         plt.xlabel('RMSE')
         plt.legend()
 
         plt.subplot(1, 2, 2)
+        for name, group in df_mode:
+            plt.hist(group['MAE'], bins=bins_mae, alpha=0.5, label=name, edgecolor='black')
+        plt.title('MAE by Data Mode')
+        plt.xlabel('MAE')
+        plt.legend()
+
+        plt.tight_layout()
+        plt.savefig('scripts/RMSE_MAE_hist_by_data_mode.png')
+
+        plt.figure(figsize=(12,6))
+
+        plt.subplot(1, 2, 1)
+        data = [group['RMSE'].values for name, group in df_mode]
+        plt.boxplot(data, labels=df_mode.groups.keys())
+        plt.ylim(bottom=0)
+        plt.title('RMSE by Data Mode')
+        plt.xlabel('Data Mode')
+        plt.ylabel('RMSE')
+
+        plt.subplot(1, 2, 2)
         data = [group['MAE'].values for name, group in df_mode]
         plt.boxplot(data, labels=df_mode.groups.keys())
+        plt.ylim(bottom=0)
         plt.title('MAE by Data Mode')
         plt.xlabel('Data Mode')
         plt.ylabel('MAE')
 
         plt.tight_layout()
-        plt.savefig('scripts/RMSE_MAE_by_data_mode.png')
+        plt.savefig('scripts/RMSE_MAE_boxplots_by_data_mode.png')
 
 def main():
     """
     Main function to orchestrate the evaluation process.
     """
 
-    experiments_folder = '/scratch2/arrueegg/WP2/GIM_fusion_VLBI/experiments/'
+    experiments_folder = '/cluster/work/igp_psr/arrueegg/WP2/GIM_fusion_VLBI/experiments/'
     
     df = pd.DataFrame()
     for experiment_name in os.listdir(experiments_folder):
